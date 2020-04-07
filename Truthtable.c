457395b51265b348327a7d678de47978ba50c887
Abid Hussain <61841766+ABID-glitch@users.noreply.github.com>
2020-04-08 05:10:29 +0530

    Update Truthtable.c

--- Truthtable.c
+++ Truthtable.c
@@ -1,15 +1,14 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
 int main(){
-    cout<<"X|Y|Z|X.Y|X.Y+Z"<<"\n";
+    printf("X|Y|Z|X.Y|X.Y+Z"\n");
     for(int x=0;x<2;x++){
         for(int y=0;y<2;y++){
             for(int z=0;z<2;z++){
                 int a=x*y+z;
                 if(a>1)
                 a=1;
-                cout<<"|"<<x<<"|"<<y<<"|"<<z<<"|"<<x*y<<"|"<<a;  
-                cout<<"\n";
+                printf("|",x,"|",y,"|",z,"|",x*y,"|"<<a);  
+                printf("\n");
             }
         }
     }
