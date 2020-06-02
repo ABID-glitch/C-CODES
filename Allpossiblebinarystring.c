ed0d9b62d6d083a200b190479d8917277a87a451
Abid Hussain <61841766+ABID-glitch@users.noreply.github.com>
2020-06-02 12:25:23 +0530

    Create Allpossiblebinarystring.c

---
+++ Allpossiblebinarystring.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int bin(int n,char A[]){
+if(n<1)
+    printf("\n%s",A);
+    else{
+        A[n-1]='0';
+        bin(n-1,A);
+        A[n-1]='1';
+        bin(n-1,A);
+    }
+}
+int main(){
+int bin(int,char[]);
+char A[11];
+A[10]='\0';
+bin(10,A);}
