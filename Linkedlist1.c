7248f51fa2925591658a68e3194316ff235ac9b9
Abid Hussain <61841766+ABID-glitch@users.noreply.github.com>
2020-06-05 22:04:00 +0530

    Create Linkedlist1.c

---
+++ Linkedlist1.c
@@ -0,0 +1,31 @@
+* INSERTION OF NODE AT THE END:
+#include<stdio.h>
+
+struct node{
+    int info;
+    struct node *link;
+};
+struct node *START=NULL;
+struct node* createNode()
+{
+    struct node *n;
+    n=(struct node * )malloc(sizeof(struct node));
+    return(n);
+}
+void insertNode(){
+struct node *temp, *t;
+temp=createNode();
+printf("Enter a number:");
+scanf("%d",&temp->info);
+temp->link=NULL;
+if(START==NULL)
+    START=temp;
+else{
+t=START;
+while(t->link!=NULL);
+t=t->link;
+
+t->link=temp;
+}
+}
+
