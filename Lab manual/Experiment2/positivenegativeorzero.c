8b9747131d1ca7cca024ad354cefc64f0f81fdda
Abid Hussain <61841766+ABID-glitch@users.noreply.github.com>
2020-06-10 13:09:45 +0530

    Create positivenegativeorzero.c

---
+++ Lab manual/Experiment2/positivenegativeorzero.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a;
+printf("Enter a number:");
+scanf("%d",&a);
+if(a>0){
+    printf("Number is positive");}
+if(a<0){
+    printf("Number is negative");}
+if(a==0)
+    printf("Number is equal to zero");
+}
