844d8f89b7c6fbd2f4a42938dc769c25c47b7c52
Abid Hussain <61841766+ABID-glitch@users.noreply.github.com>
2020-06-15 01:16:28 +0530

    Create Swap2.c

---
+++ Lab manual/Experiment4/Swap2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void swap(int ,int );
+int main(){
+    int x,y;
+printf("Enter two numbers");
+scanf("%d%d",&x,&y);
+swap(x,y);
+printf("x=%d and y=%d",x,y);
+}
+
+void swap(int a,int b){
+int t;
+t=a;
+a=b;
+b=t;
+}
